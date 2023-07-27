1e7e82b2b0861b0be88338e4c6ae2e3df17e38b9
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-07-27 17:55:41 +0700

    add coversation dto condition

--- src/coversation/dtos/create.dto.ts
+++ src/coversation/dtos/create.dto.ts
@@ -1,10 +1,28 @@
+import { IsString, IsNumber, IsArray, IsDate } from 'class-validator';
+
 export class CreateCoversationDto {
+  @IsString()
   name: string;
+
+  @IsString()
   avatar: string;
+
+  @IsNumber()
   type: number;
+
+  @IsArray()
+  @IsNumber({}, { each: true })
   members: number[];
+
+  @IsNumber()
   status: number;
+
+  @IsDate()
   background: Date;
+
+  @IsDate()
   last_activity: Date;
+
+  @IsDate()
   timestamp: Date;
 }
--- src/coversation/dtos/update.dto.ts
+++ src/coversation/dtos/update.dto.ts
@@ -1,11 +1,28 @@
+import { IsString, IsArray, IsNumber, IsDate } from 'class-validator';
+
 export class UpdateCoversationDto {
-  coversation_id: bigint;
+  @IsString()
   name: string;
+
+  @IsString()
   avatar: string;
+
+  @IsNumber()
   type: number;
+
+  @IsArray()
+  @IsNumber({}, { each: true })
   members: number[];
+
+  @IsNumber()
   status: number;
+
+  @IsDate()
   background: Date;
+
+  @IsDate()
   last_activity: Date;
+
+  @IsDate()
   timestamp: Date;
 }
