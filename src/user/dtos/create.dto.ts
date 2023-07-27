30e39e14f74bd4894f7c303399e885389844a9ff
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-07-27 11:53:53 +0700

    fix dto user

--- src/user/dtos/create.dto.ts
+++ src/user/dtos/create.dto.ts
@@ -1,15 +1,59 @@
-export interface CreateUserDto {
+import {
+  IsString,
+  IsEmail,
+  IsNumber,
+  IsDateString,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNumber()
+  user_id: bigint;
+
+  @IsString()
   avatar: string;
+
+  @IsString()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
   street: string;
+
+  @IsString()
   lat: string;
+
+  @IsString()
   lng: string;
+
+  @IsNumber()
   country_id: number;
+
+  @IsNumber()
   city_id: number;
+
+  @IsNumber()
   district_id: number;
+
+  @IsNumber()
   ward_id: number;
+
+  @IsString()
   phone: string;
+
+  @IsString()
   gender: string;
+
+  @IsString()
   birthday: string;
+
+  @IsDateString()
   timestamp: Date;
 }
--- src/user/dtos/update.dto.ts
+++ src/user/dtos/update.dto.ts
@@ -1,14 +1,46 @@
-export interface UpdateUserDto {
+import { IsString, IsNumber, IsDateString, IsNotEmpty } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
   avatar: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
   street: string;
+
+  @IsString()
   lat: string;
+
+  @IsString()
   lng: string;
+
+  @IsNumber()
   country_id: number;
+
+  @IsNumber()
   city_id: number;
+
+  @IsNumber()
   district_id: number;
+
+  @IsNumber()
   ward_id: number;
+
+  @IsString()
   phone: string;
+
+  @IsString()
   gender: string;
+
+  @IsString()
   birthday: string;
+
+  @IsDateString()
+  timestamp: Date;
 }
