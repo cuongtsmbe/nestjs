e10c19903d7cb81dfbb78ad2b5e3b5dfdca778b4
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-07-28 11:49:45 +0700

    fix reponse message

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -3,8 +3,9 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
-  ParseIntPipe,
   Post,
   Put,
   Query,
@@ -13,39 +14,73 @@ import { MessageService } from './message.service';
 import { CreateMessageDto } from './dtos/create.dto';
 import { MessageInterface } from './message.interface';
 import { UpdateMessageDto } from './dtos/update.dto';
+import { CoversationsService } from 'src/coversation/coversation.service';
 
 @Controller('message')
 export class MessageController {
-  constructor(private readonly messageService: MessageService) {}
+  constructor(
+    private readonly messageService: MessageService,
+    private readonly coversationsService: CoversationsService,
+  ) {}
 
   @Post()
   async create(@Body() dtoMessage: CreateMessageDto) {
+    const conversation = await this.coversationsService.find(
+      dtoMessage.coversation_id,
+    );
+    if (!conversation) {
+      throw new HttpException('Conversation not found', HttpStatus.NOT_FOUND);
+    }
+
     const res = await this.messageService.create(dtoMessage);
     if (!res) {
-      return false;
+      //500 Internal Server Error
+      throw new HttpException(
+        'Failed to create message',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    return true;
+    //201 Created
+    return {
+      status: HttpStatus.CREATED,
+      message: 'Message created successfully!',
+      data: res,
+    };
   }
 
   @Get(':message_id')
-  async findByID(@Param('message_id', ParseIntPipe) message_id: bigint) {
-    if (isNaN(Number(message_id))) {
-      return 'Invalid message_id';
-    }
+  async findByID(@Param('message_id') message_id: bigint) {
     const message: MessageInterface = await this.messageService.find(
       message_id,
     );
-    return message;
+
+    if (!message) {
+      throw new HttpException('Message not found', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Get message successfully!',
+      data: message,
+    };
   }
 
   @Get()
   async findByCoversationID(
     @Param('coversation_id') coversation_id: bigint,
-    @Query('limit', ParseIntPipe) limit: number,
+    @Query('limit') limit: number,
   ) {
+    const conversation = await this.coversationsService.find(coversation_id);
+    if (!conversation) {
+      throw new HttpException('Conversation not found', HttpStatus.NOT_FOUND);
+    }
     const messages: Array<MessageInterface> =
       await this.messageService.findByCoversationID(coversation_id, limit);
-    return messages;
+    return {
+      status: HttpStatus.CREATED,
+      message: 'Get messages by coversation successfully!',
+      data: messages,
+    };
   }
 
   @Put(':message_id')
@@ -53,13 +88,34 @@ export class MessageController {
     @Param('message_id') message_id: bigint,
     @Body() body: UpdateMessageDto,
   ) {
-    const message: any = await this.messageService.update(message_id, body);
-    return message;
+    const resultMessage: any = await this.messageService.update(
+      message_id,
+      body,
+    );
+
+    if (!resultMessage) {
+      throw new HttpException('Failed update message', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      status: HttpStatus.OK,
+      message: 'update message successfully!',
+      data: resultMessage,
+    };
   }
 
   @Delete(':message_id')
   async remove(@Param('message_id') message_id: bigint) {
     const resultDel = await this.messageService.delete(message_id);
-    return resultDel;
+
+    if (!resultDel) {
+      throw new HttpException('Message not found', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      status: HttpStatus.OK,
+      description: 'Message deleted successfully!',
+      data: resultDel,
+    };
   }
 }
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './message.entity';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
+import { CoversationModule } from 'src/coversation/coversation.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message]), CoversationModule],
   controllers: [MessageController],
   providers: [MessageService],
 })
