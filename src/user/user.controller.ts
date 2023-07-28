2adb6430ca926d767c5a8d1841938019a95365da
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-07-28 11:50:11 +0700

    fix response user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,8 +2,9 @@ import {
   Body,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
-  ParseIntPipe,
   Post,
   Put,
   Query,
@@ -20,30 +21,57 @@ export class UserController {
   @Post()
   async create(@Body() dtoUser: CreateUserDto) {
     const res = await this.userService.create(dtoUser);
+
     if (!res) {
-      return false;
+      //500 Internal Server Error
+      throw new HttpException(
+        'Failed to create user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-    return true;
+    //201 Created
+    return {
+      status: HttpStatus.CREATED,
+      message: 'User created successfully!',
+      data: res,
+    };
   }
 
   @Get(':user_id')
-  async findByUserID(@Param('user_id', ParseIntPipe) user_id: bigint) {
-    if (isNaN(Number(user_id))) {
-      return 'Invalid id';
-    }
+  async findByUserID(@Param('user_id') user_id: bigint) {
     const user: UserInterface = await this.userService.findByUserID(user_id);
-    return user;
+    if (!user) {
+      throw new HttpException('user not found', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Get user successfully!',
+      data: user,
+    };
   }
 
   @Get()
-  async findAll(@Query('limit', ParseIntPipe) limit: number) {
+  async findAll(@Query('limit') limit: number) {
     const users: UserInterface[] = await this.userService.findAll(limit);
-    return users;
+    return {
+      status: HttpStatus.CREATED,
+      message: 'Get users all successfully!',
+      data: users,
+    };
   }
 
   @Put(':user_id')
   async update(@Param('user_id') user_id: bigint, @Body() body: UpdateUserDto) {
     const user: any = await this.userService.update(user_id, body);
-    return user;
+    if (!user) {
+      throw new HttpException('Failed update user ', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+      status: HttpStatus.OK,
+      message: 'update user successfully!',
+      data: user,
+    };
   }
 }
